7f682c6519d62684d09a487d95d6a958c413157e
StanimirKD <sisostz1@gmail.com>
2020-05-11 12:04:15 +0200

    Organising update

---
+++ Assets/Scripts/backToMS1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class backToMS1 : MonoBehaviour
+{
+    public Slider bar;
+
+    void Start()
+    {
+       
+    }
+    public void Clicked()
+    {
+     
+        SceneManager.LoadScene("Achievements");
+    }
+}
+
